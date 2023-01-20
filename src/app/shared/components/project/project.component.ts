ef4304c7cffcd121687f6d829f08abd45a8c1ea9
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-01-20 17:28:23 -0300

    add the autoplay to the project component carousel with a delay of 6 seconds and loop

--- src/app/shared/components/project/project.component.ts
+++ src/app/shared/components/project/project.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LanguageSelectorService } from 'src/app/services/language-selector.service';
-import SwiperCore, { Navigation, SwiperOptions } from 'swiper';
+import SwiperCore, { Autoplay, Navigation, SwiperOptions } from 'swiper';
 import { Language } from '../../../models/language.interface';
 
-SwiperCore.use([Navigation]);
+SwiperCore.use([Navigation, Autoplay]);
 
 @Component({
   selector: 'jdv-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
 })
 export class ProjectComponent implements OnInit {
-
-  title: Language = { es: 'Proyectos donde he colaborado', en: 'Projects where I have collaborated' };
+  title: Language = {
+    es: 'Proyectos donde he colaborado',
+    en: 'Projects where I have collaborated',
+  };
   viewSite: Language = { es: 'Ir al sitio', en: 'View' };
   projects = [
     {
@@ -21,64 +23,64 @@ export class ProjectComponent implements OnInit {
       image: '../../../assets/projects-logos/tupemesa-logo.webp',
       principalImage: '../../../../assets/projects-images/tupemesa.webp',
       url: 'https://tienda.tupemesa.com.pe/',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'K-One Chile',
       image: '../../../assets/projects-logos/k1.webp',
       principalImage: '../../../../assets/projects-images/kone.webp',
       url: 'https://www.k1.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Antihuman Chile',
       image: '../../../assets/projects-logos/antihuman.webp',
       principalImage: '../../../../assets/projects-images/antihuman.webp',
       url: 'https://www.antihuman.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Bold Chile',
       image: '../../../assets/projects-logos/bold.webp',
       principalImage: '../../../../assets/projects-images/bold.webp',
       url: 'https://www.bold.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Belsport Chile',
       image: '../../../assets/projects-logos/belsport.webp',
       principalImage: '../../../../assets/projects-images/belsport.webp',
       url: 'https://www.belsport.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Bamers Chile',
       image: '../../../assets/projects-logos/bamers.webp',
       principalImage: '../../../../assets/projects-images/bamers.webp',
       url: 'https://www.bamers.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Crocs Chile',
       image: '../../../assets/projects-logos/crocs.webp',
       principalImage: '../../../../assets/projects-images/crocs.webp',
       url: 'https://www.crocs.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Aufbau Chile',
       image: '../../../assets/projects-logos/aufbau.webp',
       principalImage: '../../../../assets/projects-images/aufbau.webp',
       url: 'https://www.aufbau.cl',
-      target: '_blank'
+      target: '_blank',
     },
     {
       title: 'Promesa Ecuador',
       image: '../../../assets/projects-logos/promesa-logo.webp',
       principalImage: '../../../../assets/projects-images/promesa.webp',
       url: '',
-      target: '_blank'
-    }
+      target: '_blank',
+    },
   ];
   swiperConfig: SwiperOptions = {
     slidesPerView: 4,
@@ -106,15 +108,12 @@ export class ProjectComponent implements OnInit {
         slidesPerView: 4,
         slidesPerGroup: 4,
       },
-
     },
   };
 
   language$: Observable<string> = this.languageSelectorService.getLanguage();
 
-  constructor(private languageSelectorService: LanguageSelectorService) { }
+  constructor(private languageSelectorService: LanguageSelectorService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
