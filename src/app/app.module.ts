78286c015cc4c580b728a840d07da60b27990bc0
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-27 10:41:51 -0300

    create a scroll to top component with his styles and module, and add it to the app component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from '@angular/cdk/layout';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { ScrollToTopModule } from './shared/components/scroll-to-top/scroll-to-top.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     AppRoutingModule,
     LayoutModule,
-    SharedModule
+    SharedModule,
+    ScrollToTopModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
