af59e2a4dac8e130cf582e887bfc92c838b7e44b
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-27 11:20:32 -0300

    add the jdv-project component in the home page with his styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointService } from '../../services/breakpoint.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'jdv-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  isMobile$: Observable<boolean> = this.breakpointService.isMobile$;
+  constructor(private breakpointService: BreakpointService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
