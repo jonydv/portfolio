d93f8a622bd89425205ddf7aa510b830293bbe37
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-04-19 08:03:13 -0300

    add new skills to the project with the icons and alt text

--- src/app/shared/components/skills/skills.component.ts
+++ src/app/shared/components/skills/skills.component.ts
@@ -3,73 +3,86 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'jdv-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
-
   @Input() fromHome: boolean = false;
   skills = [
     {
-      icon: '../../../assets/icons/javascript.svg',
-      alt: 'Javascript Icon'
+      icon: '../../../assets/icons/spartacus.png',
+      alt: 'Sap Spartacus Icon',
     },
     {
       icon: '../../../assets/icons/typescript.svg',
-      alt: 'Typescript Icon'
+      alt: 'Typescript Icon',
     },
     {
       icon: '../../../assets/icons/angular.svg',
-      alt: 'Angular Icon'
+      alt: 'Angular Icon',
     },
     {
       icon: '../../../assets/icons/rxjs.png',
-      alt: 'RxJS Icon'
+      alt: 'RxJS Icon',
     },
     {
-      icon: '../../../assets/icons/reactjs.svg',
-      alt: 'React Icon'
+      icon: '../../../assets/icons/ngrx.svg',
+      alt: 'Ngrx Icon',
     },
     {
       icon: '../../../assets/icons/nodejs.svg',
-      alt: 'NodeJs Icon'
+      alt: 'NodeJs Icon',
     },
     {
       icon: '../../../assets/icons/expressjs.svg',
-      alt: 'ExpressJs Icon'
+      alt: 'ExpressJs Icon',
+    },
+    {
+      icon: '../../../assets/icons/javascript.svg',
+      alt: 'Javascript Icon',
+    },
+    {
+      icon: '../../../assets/icons/reactjs.svg',
+      alt: 'React Icon',
     },
     {
       icon: '../../../assets/icons/sass.svg',
-      alt: 'Sass Icon'
+      alt: 'Sass Icon',
     },
     {
       icon: '../../../assets/icons/css.svg',
-      alt: 'Css Icon'
+      alt: 'Css Icon',
     },
     {
       icon: '../../../assets/icons/html.svg',
-      alt: 'Html Icon'
+      alt: 'Html Icon',
     },
     {
       icon: '../../../assets/icons/mongodb.svg',
-      alt: 'MongoDb Icon'
+      alt: 'MongoDb Icon',
     },
     {
       icon: '../../../assets/icons/mysql.svg',
-      alt: 'MySql Icon'
+      alt: 'MySql Icon',
     },
     {
       icon: '../../../assets/icons/git.svg',
-      alt: 'Git Icon'
+      alt: 'Git Icon',
     },
     {
       icon: '../../../assets/icons/jira.svg',
-      alt: 'Jira Icon'
+      alt: 'Jira Icon',
+    },
+    {
+      icon: '../../../assets/icons/karma.webp',
+      alt: 'Karma Icon',
+    },
+    {
+      icon: '../../../assets/icons/jasmine.svg',
+      alt: 'Jasmine Icon',
     },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
